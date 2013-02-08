b27eed0d72223973cc97c4a2e0a0914ad08edcac
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-02-08 11:54:33 +0100

    first steps in evaluation

--- interpreter.hs
+++ interpreter.hs
@@ -175,13 +175,33 @@ parseExpr = parseAtom
                  char ')'
                  return x
 
-readExpr :: String -> String
-readExpr input = case parse parseExpr "lisp" input of
-    Left err -> "No match: " ++ show err
-    Right val -> "Found value"
+-- Print LispVal
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+-- Evaluation
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn (readExpr (args !! 0))
-          
+main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
+
